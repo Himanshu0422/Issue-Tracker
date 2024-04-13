c4b14833ce2c058209716388a5aaf897d09b7fa5
Himanshu0422 <himanshumittal035@gmail.com>
2024-04-13 10:56:39 +0530

    edit and show issue page

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBadge';
+import { Issue } from '@prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import Markdown from 'react-markdown';
+
+const IssueDetails = ({ issue }: { issue: Issue }) => {
+    return (
+        <div>
+            <Heading>{issue.title}</Heading>
+            <Flex className='space-x-3 my-2'>
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createdAt.toDateString()}</Text>
+            </Flex>
+            <Card className='prose'>
+                <Markdown>{issue.description}</Markdown>
+            </Card>
+        </div>
+    )
+}
+
+export default IssueDetails
---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,33 @@
+import prisma from '@/prisma/client';
+import { Box, Grid } from '@radix-ui/themes';
+import delay from 'delay';
+import { notFound } from 'next/navigation';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
+
+interface Props{
+    params: { id: string }
+}
+
+const IssueDetailsPage = async ({params}: Props) => {
+
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(params.id) }
+    })
+
+    if(!issue) notFound();
+    await delay(2000);
+
+    return (
+        <Grid columns={{initial: '1', md: '2'}} gap='5'>
+            <Box>
+                <IssueDetails issue={issue} />
+            </Box>
+            <Box>
+                <EditIssueButton issueId={issue.id} />
+            </Box>
+        </Grid>
+    )
+}
+
+export default IssueDetailsPage;
