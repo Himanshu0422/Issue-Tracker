b1e20128f5e0b65a26ee2108e57063b3f4e9dc92
Himanshu0422 <himanshumittal035@gmail.com>
2024-04-14 22:49:34 +0530

    query client and authprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import '@radix-ui/themes/styles.css';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from '@radix-ui/themes';
+import { Container, Theme } from '@radix-ui/themes';
 import "./globals.css";
 import Navbar from "./Navbar";
+import AuthProvider from './auth/Provider';
+import QueryClientProvider from '@/QueryClientProvider';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,12 +22,18 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				<Theme>
-					<Navbar />
-					<main className='p-5'>
-						{children}
-					</main>
-				</Theme>
+				<QueryClientProvider>
+				<AuthProvider>
+					<Theme>
+						<Navbar />
+						<main className='p-5'>
+							<Container>
+								{children}
+							</Container>
+						</main>
+					</Theme>
+				</AuthProvider>
+				</QueryClientProvider>
 			</body>
 		</html>
 	);
