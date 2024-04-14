6aa9ac77de03f5714cb8edc9fbe2b11ee02c50fd
Himanshu0422 <himanshumittal035@gmail.com>
2024-04-14 22:52:13 +0530

    assign issue and delete issue

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,33 +1,52 @@
 import prisma from '@/prisma/client';
-import { Box, Grid } from '@radix-ui/themes';
-import delay from 'delay';
+import { Box, Flex, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
+import DeleteIssueButton from './DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOption';
+import AssigneeSelect from './AssigneeSelect';
+import { cache } from 'react';
 
-interface Props{
-    params: { id: string }
+interface Props {
+    params: { id: string };
 }
 
-const IssueDetailsPage = async ({params}: Props) => {
+const fetchUser = cache((issueId: number) => prisma.issue.findUnique({ where: { id: issueId } }));
 
-    const issue = await prisma.issue.findUnique({
-        where: { id: parseInt(params.id) }
-    })
+const IssueDetailPage = async ({ params }: Props) => {
+    const session = await getServerSession(authOptions);
 
-    if(!issue) notFound();
-    await delay(2000);
+    const issue = await fetchUser(parseInt(params.id));
+
+    if (!issue) notFound();
 
     return (
-        <Grid columns={{initial: '1', md: '2'}} gap='5'>
-            <Box>
+        <Grid columns={{ initial: '1', sm: '5' }} gap="5">
+            <Box className="md:col-span-4">
                 <IssueDetails issue={issue} />
             </Box>
-            <Box>
-                <EditIssueButton issueId={issue.id} />
-            </Box>
+            {session && (
+                <Box>
+                    <Flex direction="column" gap="4">
+                        <AssigneeSelect issue={issue} />
+                        <EditIssueButton issueId={issue.id} />
+                        <DeleteIssueButton issueId={issue.id} />
+                    </Flex>
+                </Box>
+            )}
         </Grid>
-    )
+    );
+};
+
+export async function generateMetadata({ params }: Props) {
+    const issue = await fetchUser(parseInt(params.id));
+
+    return {
+        title: issue?.title,
+        description: 'Details of issue ' + issue?.id
+    }
 }
 
-export default IssueDetailsPage;
+export default IssueDetailPage;
